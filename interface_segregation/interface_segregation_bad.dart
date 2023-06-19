e5884427377fb4eb57868761e49c0f13beb44834
hatanvir <tanvirhasan553@gmail.com>
2023-06-19 23:39:39 +0600

    interface segregation principle added

---
+++ interface_segregation/interface_segregation_bad.dart
@@ -0,0 +1,39 @@
+///The Interface Segregation Principle (ISP) states that clients should not be forced to depend upon interface members they do not use.
+///Interface segregation principle Bad
+
+abstract class BaseFetch{
+  void saveUserInfoFromLocalDb();
+  void getUserInfoFromLocalDb();
+
+  void saveUserInfoFromRemote();
+  void getUserInfoFromRemote();
+}
+
+///here service is only responsible for handling user-related server requests.
+///but unfortunately there are some extra methods implemented like fetching data from local db, sd cards
+///those methods are unusual, we are not gonna use those anymore
+///and this breaks ISP principle
+///we will solve it on interface_segregation_good file
+
+class UserService extends BaseFetch{
+  @override
+  void getUserInfoFromLocalDb() {
+    print('This method is not relevant to service class');
+  }
+
+  @override
+  void getUserInfoFromRemote() {
+    print('Fetching data from server');
+  }
+
+  @override
+  void saveUserInfoFromLocalDb() {
+    print('This method is not relevant to service class');
+  }
+
+  @override
+  void saveUserInfoFromRemote() {
+    print('Saving data on server');
+  }
+
+}
---
+++ interface_segregation/interface_segregation_good.dart
@@ -0,0 +1,42 @@
+
+///The Interface Segregation Principle (ISP) states that clients should not be forced to depend upon interface members they do not use.
+///Interface segregation principle Good
+
+abstract class LocalFetch{
+  void saveUserInfoFromLocalDb();
+  void getUserInfoFromLocalDb();
+}
+
+abstract class RemoteFetch{
+  void saveUserInfoFromRemote();
+  void getUserInfoFromRemote();
+}
+
+class UserService extends RemoteFetch{
+  @override
+  void getUserInfoFromRemote() {
+    print('Fetching data from server');
+  }
+
+  @override
+  void saveUserInfoFromRemote() {
+    print('Saving data on server');
+  }
+
+}
+
+class UserDao extends LocalFetch{
+  @override
+  void getUserInfoFromLocalDb() {
+    print('Fetching data from local db');
+  }
+
+  @override
+  void saveUserInfoFromLocalDb() {
+    print('saving data on local db');
+  }
+}
+
+///here we segregate our method into two interface
+///one for local and another one is for server
+///and if we use those and no irrelevant method needs to implement
